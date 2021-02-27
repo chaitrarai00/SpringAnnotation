30961534e5ab9de66999b540a3a90670e03609e2
chaitrarai00 <chaitrarai00@gmail.com>
2021-02-27 23:50:01 +0530

    property file and component classes

---
+++ src/main/java/com/example/demo/components/DataBaseDriver.java
@@ -0,0 +1,10 @@
+package com.example.demo.components;
+/*
+ * DataBase interface for implementation 
+ * by all components to get information on databases
+ */
+public interface DataBaseDriver {
+	
+	public String getInfo();
+	
+}
